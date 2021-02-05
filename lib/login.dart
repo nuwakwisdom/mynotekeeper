23431f9cec07e5111b7e6656ab457df2603fee27
wisdomNuwak <nuwakwisdom25@gmail.com>
2021-02-04 22:44:58 -0800

    changed name

--- lib/login.dart
+++ lib/login.dart
@@ -1,7 +1,7 @@
 import 'dart:convert';
 
 import 'package:flutter/material.dart';
-import 'package:notekeeper/formpage.dart';
+import 'package:notekeeper/form.dart';
 
 import 'home.dart';
 
@@ -11,6 +11,9 @@ class Login extends StatefulWidget {
 }
 
 class _LoginState extends State<Login> {
+  String text = "Welcome back";
+  String subText = "Sign in contine";
+  int count = 0;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -18,122 +21,136 @@ class _LoginState extends State<Login> {
         title: Text('Login'),
       ),
       body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            SizedBox(
-              height: 30,
-            ),
-            Text(
-              'Welcome back',
-              style: TextStyle(
-                color: Colors.black,
-                fontSize: 30,
-                fontWeight: FontWeight.bold,
+        child: SingleChildScrollView(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              SizedBox(
+                height: 30,
               ),
-              textAlign: TextAlign.left,
-            ),
-            Text(
-              'Sign in to continue',
-              style: TextStyle(
-                color: Colors.black,
-                fontSize: 20,
-                fontWeight: FontWeight.normal,
+              Text(
+                text,
+                style: TextStyle(
+                  color: Colors.black,
+                  fontSize: 30,
+                  fontWeight: FontWeight.bold,
+                ),
+                textAlign: TextAlign.left,
               ),
-              textAlign: TextAlign.left,
-            ),
-            SizedBox(
-              height: 30,
-            ),
-            Container(
-              width: 300,
-              height: 50,
-              child: TextFormField(
-                decoration: InputDecoration(
-                  icon: Icon(
-                    Icons.mail,
-                    color: Colors.deepPurple,
+              Text(
+                "You have clicked the button " + count.toString() + " times",
+                style: TextStyle(
+                  color: Colors.black,
+                  fontSize: 20,
+                  fontWeight: FontWeight.normal,
+                ),
+                textAlign: TextAlign.left,
+              ),
+              SizedBox(
+                height: 30,
+              ),
+              Container(
+                width: 300,
+                height: 50,
+                child: TextFormField(
+                  decoration: InputDecoration(
+                    icon: Icon(
+                      Icons.mail,
+                      color: Colors.deepPurple,
+                    ),
+                    hintText: 'Enter email',
                   ),
-                  hintText: 'Enter email',
                 ),
               ),
-            ),
-            SizedBox(
-              height: 30,
-            ),
-            Container(
-              width: 300,
-              height: 50,
-              child: TextFormField(
-                decoration: InputDecoration(
-                  icon: Icon(
-                    Icons.remove_red_eye,
+              SizedBox(
+                height: 30,
+              ),
+              Container(
+                width: 300,
+                height: 50,
+                child: TextFormField(
+                  decoration: InputDecoration(
+                    icon: Icon(
+                      Icons.remove_red_eye,
+                    ),
+                    hintText: 'Enter passwrod',
+                    border: InputBorder.none,
                   ),
-                  hintText: 'Enter passwrod',
-                  border: InputBorder.none,
                 ),
               ),
-            ),
-            SizedBox(
-              height: 20,
-            ),
-            Align(
-              alignment: Alignment.bottomRight,
-              child: Padding(
-                padding: const EdgeInsets.all(20),
-                child: Text(
-                  'Forgot Password?',
+              SizedBox(
+                height: 20,
+              ),
+              Align(
+                alignment: Alignment.bottomRight,
+                child: Padding(
+                  padding: const EdgeInsets.all(20),
+                  child: Text(
+                    'Forgot Password?',
+                    style: TextStyle(
+                      fontSize: 15,
+                    ),
+                  ),
+                ),
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              SizedBox(
+                width: 280,
+                height: 50,
+                child: RaisedButton(
+                  onPressed: () {
+                    print(text);
+                    setState(() {
+                      count = count + 1;
+                      text = "Hello Wisdom!";
+                      subText = "You have successfullly login";
+                    });
+                    print(text);
+                    // Navigator.push(
+                    //   context,
+                    //   MaterialPageRoute(
+                    //     builder: (_) => Home(),
+                    //   ),
+                    // );
+                  },
+                  color: Colors.deepPurple,
+                  child: Text(
+                    'Sign in',
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 25,
+                    ),
+                  ),
+                ),
+              ),
+              SizedBox(
+                height: 30,
+              ),
+              Text('New User?',
                   style: TextStyle(
-                    fontSize: 15,
-                  ),
-                ),
-              ),
-            ),
-            SizedBox(
-              height: 20,
-            ),
-            SizedBox(
-              width: 280,
-              height: 50,
-              child: RaisedButton(
-                onPressed: () {
+                    fontSize: 16,
+                  )),
+              GestureDetector(
+                onTap: () {
                   Navigator.push(
                       context,
                       MaterialPageRoute(
-                        builder: (_) => Home(),
+                        builder: (_) => FormPage(),
                       ));
                 },
-                color: Colors.deepPurple,
                 child: Text(
-                  'Sign in',
+                  'Create Account',
                   style: TextStyle(
-                    color: Colors.white,
-                    fontSize: 25,
+                    color: Colors.purple,
+                    fontWeight: FontWeight.bold,
+                    fontSize: 18,
                   ),
                 ),
               ),
-            ),
-            SizedBox(
-              height: 30,
-            ),
-            Text('New User?',
-                style: TextStyle(
-                  fontSize: 16,
-                )),
-            GestureDetector(
-              onTap: () {
-                Navigator.pushNamed(context, '/FormPage');
-              },
-              child: Text(
-                'Create Account',
-                style: TextStyle(
-                  color: Colors.purple,
-                  fontWeight: FontWeight.bold,
-                  fontSize: 18,
-                ),
-              ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
